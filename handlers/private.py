2598646500b09ebddfce45da0fb05d67d6b34c7a
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-08 10:46:54 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/c247284e8ddc50f01a314.png",
+        photo=f"https://telegra.ph/file/f50911b821563d74eaa55.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜**""",
     reply_markup=InlineKeyboardMarkup(
             [
@@ -38,7 +38,7 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/c247284e8ddc50f01a314.png",
+        photo=f"https://telegra.ph/file/f50911b821563d74eaa55.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
